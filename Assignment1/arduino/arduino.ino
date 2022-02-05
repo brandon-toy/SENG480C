1bf303f3c7f664dc1ca401e0f350d74378e41839
brandon-toy <brandontoy@uvic.ca>
2022-02-05 15:50:33 -0800

    update project doc add steps to recreate

--- Assignment1/arduino/arduino.ino
+++ Assignment1/arduino/arduino.ino
@@ -1,22 +1,24 @@
 #include <Ultrasonic.h>
-const int trigger = 1;
-const int echo = 2; 
+const int trigger = 12;
+const int echo = 13; 
 
-Ultrasonic ultrasonic_left(left_trigger, left_echo);
+Ultrasonic ultrasonic(trigger, echo);
 
 void setup() {
   Serial.begin(9600); 
-    
   pinMode(trigger, OUTPUT); 
   pinMode(echo, INPUT); 
 
 }
 
 void loop() { 
-  Serial.print("Sensor: ");
-  Serial.print(ultrasonic_left.read());
-  Serial.println("cm");
-  delay(1000);
+  int distance = ultrasonic.read();
+  Serial.println(distance);
+  if(distance < 50) {
+    // advance next slide
+    Serial.println("Next");
+  }
+  delay(100);
 }
 
 
---
+++ Assignment1/arduino/arudino.py
@@ -0,0 +1,11 @@
+import serial
+import pyautogui
+arduino = serial.Serial('/dev/cu.usbmodem1101', 9600)
+def write_read(x):
+    data = arduino.readline()
+    return data
+while True:
+    value = str(write_read(1))
+    print(value)
+    if 'Next' in value:
+        pyautogui.hotkey('right')
